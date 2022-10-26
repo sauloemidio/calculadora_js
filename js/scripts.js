0622d2999bff534bcf7da8a11aedc4a98b7ad5d4
sauloemidio <sauloemidio@gmail.com>
2022-10-26 16:37:01 -0300

    initializing javascript

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+const previousOperationText = document.querySelector('#previous-operation')
+const currentOperationText = document.querySelector('#current-operation')
+const buttons = document.querySelectorAll('#buttons-container button')
+
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText
+    this.currentOperationText = currentOperationText
+    this.currentOperation = ''
+  }
+
+  addDigit(digit) {
+    this.currentOperation = digit
+    this.updateScreen()
+  }
+  updateScreen() {
+    this.currentOperationText.innerText += this.currentOperation
+  }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText)
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const value = e.target.innerText
+
+    if (+value >= 0 || value === '.') {
+      calc.addDigit(value)
+    } else {
+    }
+  })
+})
