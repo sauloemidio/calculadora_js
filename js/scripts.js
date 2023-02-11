fb238d0d0404591732c5e64d02f15646c08fe1f3
Saulo Emídio <sauloemidio@gmail.com>
2023-02-11 14:36:23 -0300

    implementando a lógica das operaçãoes

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,77 @@ class Calculator {
   constructor(previousOperationText, currentOperationText) {
     this.previousOperationText = previousOperationText
     this.currentOperationText = currentOperationText
-    this.currentOperation = ''
+    this.currentOperation = ""
   }
 
   addDigit(digit) {
+    if(digit === "." && this.currentOperationText.innerText.includes(".")){
+      return;
+    }
     this.currentOperation = digit
     this.updateScreen()
   }
-  updateScreen() {
-    this.currentOperationText.innerText += this.currentOperation
+ 
+  
+  processOperation (operation) {
+
+    let operationValue;
+    const previus = +this.previousOperationText.innerText.split(" ")[0];
+    const current = +this.currentOperationText.innerText;
+
+
+    switch(operation){
+      case "+":
+        operationValue = previus + current;
+        this.updateScreen(operationValue, operation, current, previus);
+        break;
+        case "-":
+        operationValue = previus - current;
+        this.updateScreen(operationValue, operation, current, previus);
+        break;
+        case "/":
+        operationValue = previus / current;
+        this.updateScreen(operationValue, operation, current, previus);
+        break;
+        case "*":
+        operationValue = previus * current;
+        this.updateScreen(operationValue, operation, current, previus);
+        break;
+      default:
+        return;
+    }
+}
+
+updateScreen(operationValue = null, operation = null, current = null, previus = null) {
+  
+
+  if(operationValue === null) {
+    this.currentOperationText.innerText += this.currentOperation;
+  } else{
+    if(previus === 0 ){
+      operationValue =  current
+    }
+    this.previousOperationText.innerText = `${operationValue} ${operation}` 
+    this.currentOperationText.innerText = "";
+
   }
 }
+
+
+
+}
+
 
 const calc = new Calculator(previousOperationText, currentOperationText)
 
 buttons.forEach(btn => {
-  btn.addEventListener('click', e => {
+  btn.addEventListener('click', (e) => {
     const value = e.target.innerText
 
     if (+value >= 0 || value === '.') {
       calc.addDigit(value)
-    } else {
+    }else {
+      calc.processOperation(value)
     }
   })
 })
